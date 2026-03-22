1ae53af2e1e6fd8fdcabb75e6eb1806931d29215
noa-raz <noa.raz@plus500.com>
2026-03-22 20:57:04 +0200

    revert: remove Google auth redirect flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,6 @@ import { LoginPage } from './pages/LoginPage'
 import { RegisterPage } from './pages/RegisterPage'
 import { SetupPage } from './pages/SetupPage'
 import { SmartRedirect } from './components/SmartRedirect'
-import { AuthCallbackPage } from './pages/AuthCallbackPage'
 
 function App() {
   return (
@@ -38,7 +37,6 @@ function App() {
               }
             />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/auth/callback" element={<AuthCallbackPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/setup" element={<SetupPage />} />
             <Route
